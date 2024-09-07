48787a9022cb47896e90af0a07b258574689dd03
Max Hager <68147541+yachty66@users.noreply.github.com>
2024-09-07 16:19:54 +0200

    Refactor check_time function: comment out time checking logic and related functionality for debugging purposes.

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ load_dotenv()
 
 def check_time(username, token):
     last_event_time = fetch_last_event_local_time(username, token)
-    # Extract timezone offset from the given datetime string
-    timezone_offset = last_event_time.utcoffset().total_seconds() / 60  # offset in minutes
-    timezone = pytz.FixedOffset(timezone_offset)
+    print("last_event_time", last_event_time)
 
-    # Get the current time in the extracted timezone
-    current_time = datetime.now(timezone)
-    print("current_time", current_time.hour, current_time.minute)
+    # print("last_event_time", last_event_time)
+    # # Extract timezone offset from the given datetime string
+    # timezone_offset = last_event_time.utcoffset().total_seconds() / 60  # offset in minutes
+    # timezone = pytz.FixedOffset(timezone_offset)
 
-    # Check if the current time is within 5 minutes of 16:07
-    print("checking time")
-    if current_time.hour == 16 and current_time.minute in range(2, 20):
-        print("it's time")
-        return True
-    print("not time yet")
-    return False
+    # # Get the current time in the extracted timezone
+    # current_time = datetime.now(timezone)
+    # print("current_time", current_time.hour, current_time.minute)
+
+    # # Check if the current time is within 5 minutes of 16:07
+    # print("checking time")
+    # if current_time.hour == 16 and current_time.minute in range(2, 20):
+    #     print("it's time")
+    #     return True
+    # print("not time yet")
+    # return False
 
 if __name__ == "__main__":
     print("starting")
@@ -34,11 +37,11 @@ if __name__ == "__main__":
 
     if check_time(username, token):
         print("collecting activity")
-        activity_summary = collect_github_activity(username, token)
-        print(activity_summary)
+        # activity_summary = collect_github_activity(username, token)
+        # print(activity_summary)
 
-        # Convert activity_summary to JSON string
-        activity_summary_json = json.dumps(activity_summary)
+        # # Convert activity_summary to JSON string
+        # activity_summary_json = json.dumps(activity_summary)
 
-        # Call the telegram.py script with the activity summary
-        subprocess.run(["python", "telegram_bot.py", activity_summary_json])
+        # # Call the telegram.py script with the activity summary
+        # subprocess.run(["python", "telegram_bot.py", activity_summary_json])
